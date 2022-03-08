4b946b2d8a09e957a2b1cd9d569b6ec534e038a0
lewisdavidson <lewisdavidson8@gmail.com>
2022-03-08 17:16:37 +0000

    Fixed data not submitting to console

---
+++ components/ContactForm.styled.tsx
@@ -0,0 +1,105 @@
+import styled from "styled-components";
+import { keyframes } from "styled-components";
+
+export const FormGroup = styled.div`
+  color: var(--color-primary, #3992ff);
+  display: block;
+  width: 100%;
+  margin: 50px auto;
+`;
+
+export const Label = styled.label`
+  margin-bottom: 0.5em;
+  color: var(--color-primary, #3992ff);
+  display: block;
+`;
+
+export const Input = styled.input`
+  padding: 1em;
+  color: #000;
+  background: var(--color-icons, #f8f8f8);
+  border: none;
+  border-radius: 3px;
+  width: 100%;
+  margin-bottom: 0.5em;
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+export const TextArea = styled.textarea`
+  width: 100%;
+`;
+
+export const Message = styled.label`
+  margin-bottom: 0.5em;
+  color: var(--color-primary, #3992ff);
+  display: block;
+`;
+
+export const flyOut = keyframes`
+    from {
+        transform: translateY(0.1em);
+    }
+
+    to {
+        transform: translateY(-0.1em);
+    }
+`;
+
+export const SubmitBtn = styled.button`
+  font-size: 1.5rem;
+  font-family: inherit;
+  cursor: pointer;
+  background-color: var(--color-primary, #3992ff);
+  color: #fff;
+  padding: 0.7em 1em 0.7em 0.9em;
+  display: flex;
+  align-items: center;
+  border-radius: 1rem;
+  overflow: hidden;
+  transition: all 0.2s;
+
+  span {
+    display: block;
+    margin-left: 0.3em;
+    transition: all 0.3s ease-in-out;
+  }
+
+  svg {
+    display: block;
+    transform-origin: center center;
+    transition: transform 0.3s ease-in-out;
+  }
+
+  &:hover .svg-wrapper {
+    animation: ${flyOut} 0.6s ease-in-out infinite alternate;
+  }
+
+  &:hover svg {
+    transform: translateX(1.2em) rotate(45deg) scale(1.1);
+  }
+
+  &:hover span {
+    transform: translateX(5em);
+  }
+
+  &:active {
+    transform: scale(0.95);
+  }
+`;
+
+export const SpamField = styled.p`
+  opacity: 0;
+  position: absolute;
+  height: 0;
+  width: 0;
+
+  label {
+    opacity: 0;
+  }
+  input {
+    opacity: 0;
+  }
+`;
--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,134 +1,58 @@
-import styled from "styled-components";
-import { keyframes } from "styled-components";
+import {
+  FormGroup,
+  Label,
+  Input,
+  TextArea,
+  Message,
+  SubmitBtn,
+  SpamField,
+} from "./ContactForm.styled";
+import { useForm, SubmitHandler } from "react-hook-form";
 
-const FormGroup = styled.div`
-  color: var(--color-primary, #3992ff);
-  display: block;
-  width: 100%;
-  margin: 50px auto;
-`;
-
-const Label = styled.label`
-  margin-bottom: 0.5em;
-  color: var(--color-primary, #3992ff);
-  display: block;
-`;
-
-const Input = styled.input`
-  padding: 0.5em;
-  color: #000;
-  background: var(--color-icons, #f8f8f8);
-  border: none;
-  border-radius: 3px;
-  width: 100%;
-  margin-bottom: 0.5em;
-`;
-
-const TextArea = styled.textarea`
-  width: 100%;
-`;
-
-const Message = styled.label`
-  margin-bottom: 0.5em;
-  color: var(--color-primary, #3992ff);
-  display: block;
-`;
-
-const flyOut = keyframes`
-    from {
-        transform: translateY(0.1em);
-    }
-
-    to {
-        transform: translateY(-0.1em);
-    }
-`;
-
-const SubmitBtn = styled.button`
-  font-size: 1.5rem;
-  font-family: inherit;
-  cursor: pointer;
-  background-color: var(--color-primary, #3992ff);
-  color: #fff;
-  padding: 0.7em 1em 0.7em 0.9em;
-  display: flex;
-  align-items: center;
-  border-radius: 1rem;
-  overflow: hidden;
-  transition: all 0.2s;
-
-  span {
-    display: block;
-    margin-left: 0.3em;
-    transition: all 0.3s ease-in-out;
-  }
-
-  svg {
-    display: block;
-    transform-origin: center center;
-    transition: transform 0.3s ease-in-out;
-  }
-
-  &:hover .svg-wrapper {
-    animation: ${flyOut} 0.6s ease-in-out infinite alternate;
-  }
-
-  &:hover svg {
-    transform: translateX(1.2em) rotate(45deg) scale(1.1);
-  }
-
-  &:hover span {
-    transform: translateX(5em);
-  }
-
-  &:active {
-    transform: scale(0.95);
-  }
-`;
-
-const SpamField = styled.p`
-  opacity: 0;
-  position: absolute;
-  height: 0;
-  width: 0;
-
-  label {
-    opacity: 0;
-  }
-  input {
-    opacity: 0;
-  }
-`;
+type FormValues = {
+  name: string;
+  email: string;
+  message: string;
+};
 
 const ContactForm = () => {
+  const { register, handleSubmit } = useForm<FormValues>();
+  const onSubmit: SubmitHandler<FormValues> = (data) => console.log(data);
   return (
     <form
       name="contact"
+      id="contact"
       method="POST"
-      data-netlify="true"
-      data-netlify-honeypot="fullname"
+      onSubmit={handleSubmit(onSubmit)}
     >
-      <Input type="hidden" name="form-name" value="contact" />
-      <SpamField>
-        <label aria-hidden="true">
-          Full Name
-          <input name="fullname" aria-hidden="true" />
-        </label>
-      </SpamField>
       <FormGroup>
-        <Label htmlFor="label">Name</Label>
-        <Input id="label" type="text" required />
+        <Label htmlFor="name">Name</Label>
+        <Input
+          id="name"
+          type="text"
+          {...register("name", { required: true })}
+        />
         {/* <Message>This is the validation message</Message> */}
       </FormGroup>
       <FormGroup>
         <Label htmlFor="email">Email</Label>
-        <Input id="email" type="email" required />
+        <Input
+          id="email"
+          type="email"
+          {...(register("email"), { required: true })}
+        />
       </FormGroup>
       <FormGroup>
         <Label htmlFor="message">Message</Label>
-        <TextArea id="message" rows={5} required />
+        <TextArea
+          id="message"
+          rows={5}
+          {...(register("message"), { required: true })}
+        />
       </FormGroup>
-      <SubmitBtn>
+
+      {/* <input type="submit" /> */}
+      <SubmitBtn type="submit" form="contact">
         <div className="svg-wrapper-1">
           <div className="svg-wrapper">
             <svg
